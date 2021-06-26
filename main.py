cd1f26741ba0e48cdf755e24a18edded44e80eb6
Bamimore-Tomi <tomibami2020@gmail.com>
2021-06-26 11:14:00 -0400

    adding utility to create new wallet

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import os, logging
-from datetime  import datetime
+from datetime import datetime
 import pytz
 from telegram.ext import Updater
-from telegram.ext import CommandHandler , MessageHandler, Filters , ConversationHandler,CallbackQueryHandler
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup
+from telegram.ext import (
+    CommandHandler,
+    MessageHandler,
+    Filters,
+    ConversationHandler,
+    CallbackQueryHandler,
+)
+from telegram import (
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    KeyboardButton,
+    ReplyKeyboardMarkup,
+)
 import telegram
 from faunadb import query as q
 from faunadb.objects import Ref
@@ -13,12 +24,16 @@ import utils
 load_dotenv()
 client = utils.load_db()
 messages = utils.load_messages()
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.DEBUG)
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.DEBUG
+)
+ASK_WALLET_NAME = range(0)
+
 
 def start(update, context):
     chat_id = update.effective_chat.id
     try:
-        user = client.query(q.get(q.match(q.index("user_index"),chat_id)))
+        user = client.query(q.get(q.match(q.index("user_index"), chat_id)))
     except Exception as e:
         print(e)
         user = client.query(
@@ -26,7 +41,7 @@ def start(update, context):
                 q.collection("user"),
                 {
                     "data": {
-                        "user_id":chat_id,
+                        "user_id": chat_id,
                         "username": update.message.chat.username,
                         "firstname": update.message.chat.first_name,
                         "lastname": update.message.chat.last_name,
@@ -35,31 +50,62 @@ def start(update, context):
                 },
             )
         )
-        context.bot.send_message(chat_id=chat_id, text=messages["welcome"],parse_mode=telegram.ParseMode.MARKDOWN)
-        context.bot.send_message(chat_id=chat_id,text=messages["help"],parse_mode=telegram.ParseMode.MARKDOWN)
+        context.bot.send_message(
+            chat_id=chat_id,
+            text=messages["welcome"],
+            parse_mode=telegram.ParseMode.MARKDOWN,
+        )
+        context.bot.send_message(
+            chat_id=chat_id,
+            text=messages["help"],
+            parse_mode=telegram.ParseMode.MARKDOWN,
+        )
 
-def helper(update,context):
-    chat_id = update.effective_chat.id
-    context.bot.send_message(chat_id=chat_id,text=messages["help"],parse_mode=telegram.ParseMode.MARKDOWN)
 
-def newWallet(update,context):
+def helper(update, context):
     chat_id = update.effective_chat.id
-    context.bot.send_message(chat_id=chat_id,text=messages["newWallet"])
+    context.bot.send_message(
+        chat_id=chat_id, text=messages["help"], parse_mode=telegram.ParseMode.MARKDOWN
+    )
 
-def createWallet(update, context):
+
+def new_wallet(update, context):
     chat_id = update.effective_chat.id
+    context.bot.send_message(
+        chat_id=chat_id,
+        text=messages["newWallet"],
+        parse_mode=telegram.ParseMode.MARKDOWN,
+    )
+    return ASK_WALLET_NAME
+
+
+def create_wallet(update, context):
+    chat_id = update.effective_chat.id
+    valid, wallet_name = utils.wallet_name_validator(update.message.text)
+    while valid is False:
+        context.bot.send_message(chat_id=chat_id, text=messages["newWallet"])
+
 
 def main():
     updater = Updater(token=os.getenv("TOKEN"), use_context=True)
     dispatcher = updater.dispatcher
-    entry_ = CommandHandler("start",start)
+    entry_ = CommandHandler("start", start)
     helper_ = CommandHandler("help", helper)
+    new_wallet_conv = ConversationHandler(
+        entry_points=[CommandHandler("NewWallet", new_wallet)],
+        states={
+            ASK_WALLET_NAME: [MessageHandler(Filters.regex(r"\w*"), create_wallet)]
+        },
+        fallbacks=[CommandHandler("NewWallet", new_wallet)],
+    )
 
     dispatcher.add_handler(entry_)
     dispatcher.add_handler(helper_)
+    dispatcher.add_handler(new_wallet_conv)
 
     updater.start_polling()
     updater.idle()
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
--- utils.py
+++ utils.py
@@ -2,14 +2,82 @@ import os, json
 from faunadb import query as q
 from faunadb.objects import Ref
 from faunadb.client import FaunaClient
+from tronapi import Tron
+from tronapi.common.account import PrivateKey
 from dotenv import load_dotenv
+
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.fernet import Fernet
+
 
 def load_db():
     load_dotenv()
     client = FaunaClient(secret=os.getenv("FAUNA-KEY"))
     return client
 
+
 def load_messages():
-    f = open("messages.json","r")
+    f = open("messages.json", "r")
     messages = json.load(f)
     return messages
+
+
+def wallet_name_validator(text: str) -> str:
+    text = str(text).strip()
+    if len(text) > 12:
+        return False, None
+    if text.split(" ") > 1:
+        return False, None
+    return text
+
+
+def _generate_fernet_key(self, master_key: str, salt: str) -> str:
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA512(),
+        length=32,
+        salt=salt.encode(),
+        iterations=100000,
+        backend=default_backend(),
+    )
+    key = base64.urlsafe_b64encode(kdf.derive(master_key.encode()))
+    return key.decode("utf-8")
+
+
+def _encrypt_private_key(self, private_key: str, fernet_key: str) -> str:
+    encryptor = Fernet(fernet_key)
+    hash = encryptor.encrypt(private_key.encode())
+    return hash.decode()
+
+
+def _decrypt_private_key(self, hash, key) -> str:
+    decryptor = Fernet(key)
+    private_key = decryptor.decrypt(hash.encode())
+    return private_key.decode()
+
+
+def create_wallet(user_id, wallet_name) -> bool:
+    tron = Tron()
+    account = tron.create_account
+    address = account.address.base58
+    fernet_key = _generate_fernet_key(os.getenv("MASTER"), os.getenv("SALT"))
+    encrypted_private_key = _encrypt_private_key(account.private_key, fernet_key)
+    wallet = client.query(
+        q.create(
+            q.collection("user"),
+            {
+                "data": {
+                    "user_id": user_id,
+                    "wallet_name": wallet_name,
+                    "encrypted_private_key": encrypted_private_key,
+                    "public_key": public_key,
+                    "wallet_address": address,
+                    "wallet_account_balance": 0.0,
+                    "transactions": [],
+                    "date_generated": time.time(),
+                }
+            },
+        )
+    )
+    return True
